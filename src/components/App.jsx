b22e4a13275620eabf39cc6c85c1e1908977e91b
ArjoliZogolli <jolinjozugolli@gmail.com>
2023-06-09 14:39:16 +0200

    FilmpireNavigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+
+import userStyles from './styles';
 
 const App = () => {
+  const classes = userStyles();
+
+
   return (
-    <div>
-      App
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Router>
+        <Route exact path="/movie/:id">
+          <MovieInformation />
+          </Route>
+          <Route exact path="/actors/:id">
+          <Actors />
+          </Route>
+          <Route exact path="/">
+          <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+          <Profile />
+          </Route>
+        </Router>
+      </main>
     </div>
   )
 }
